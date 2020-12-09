9ddd73baf910a95dabe305b8ab1fe983b6ea204a
Deko Dimeskio <d.dimeski.mk@gmail.com>
2020-12-09 11:00:38 +0100

    Lab exercise

---
+++ Python/Python Fundamentals/05 Basic Syntax, Conditional Statements and Loops/Lab.py
@@ -0,0 +1,17 @@
+# 03. Word Reverse
+
+# Ver.1
+# word = "Hello World"
+# word = input()
+# for char in word:
+#     print(char)
+#
+# print(len(word))
+#
+# for index in range(len(word)-1, -1, -1):
+#     print(word[index], end='')
+
+#Ver.2
+word = input()
+print(word[::-1])
+
