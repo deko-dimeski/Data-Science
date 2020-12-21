cf018a600dacdd94fc41723cdcf6ee9b30a8f444
Deko Dimeskio <d.dimeski.mk@gmail.com>
2020-12-21 23:01:55 +0100

    blabla

--- Python/Python Fundamentals/05 Basic Syntax, Conditional Statements and Loops/05. Can't Sleep? Count Sheep.py
+++
@@ -1,5 +0,0 @@
-num_sheeps = int(input())
-
-for i in range(1, num_sheeps + 1):
-    print("%d sheep..." % i, end='')
-
---
+++ Python/Python Fundamentals/07 Data Types and Variables/02.Chars to String.py
@@ -0,0 +1,5 @@
+ch_1 = input()
+ch_2 = input()
+ch_3 = input()
+
+print(f"{ch_1}{ch_2}{ch_3}")
